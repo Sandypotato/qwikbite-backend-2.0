d534fadaa73258fc16cafb425dc8c51e44a436e2
Anders Choo <108884742+Sandypotato@users.noreply.github.com>
2025-06-19 15:45:43 +0800

    Update orderController.js 5

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -3,7 +3,7 @@ import userModel from "../models/userModel.js";
 import Stripe from "stripe";
 
 import mongoose from "mongoose";
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+//const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 
 
