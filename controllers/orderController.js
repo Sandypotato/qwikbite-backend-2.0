a476586dd8aa9bc23fccce5ee8eca1a211d6a981
Anders Choo <108884742+Sandypotato@users.noreply.github.com>
2025-06-19 15:13:41 +0800

    Update orderController.js

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -9,7 +9,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 // placing user order for frontend
 const placeOrder = async (req, res) => {
-  const frontend_url = "https://qwikbite-frontend.onrender.com/"; // Change this to your frontend URL
+  const frontend_url = "https://qwikbite-frontend.onrender.com"; // Change this to your frontend URL
   const numOrders = await orderModel.countDocuments({});
   try {
     const newOrder = new orderModel({
